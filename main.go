7180b86832da2fc68c0479cca22f37a3ca541945
nikita <7jay94@mail.ru>
2025-01-01 16:38:02 +0200

    add: added new func

--- main.go
+++ main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
-	f "fmt"
+	"fmt"
+	"math"
 )
 
+const IMT_POW = 2
+
 func main() {
-	sayHello := "Привет ты напечатал приветствие )"
-	f.Println(sayHello)
+	fmt.Println("Привет, введите ваш рост в сантиметрах и вес")
+
+	userHeight, userWeight, err := readUserParam()
+	if err != nil {
+		fmt.Println("Ошибка при вводе:", err)
+		return
+	}
+
+	imt := calculateImt(userHeight, userWeight)
+
+	fmt.Printf("Ваш индекс массы тела составляет %.2f\n", imt)
+	fmt.Printf("Длина строки Golang %d", len("Golang"))
+}
+
+func calculateImt(height float64, weight float64) float64 {
+	return weight / math.Pow(height/100, IMT_POW)
+}
+
+func readUserParam() (height float64, weight float64, err error) {
+	fmt.Print("Рост: ")
+	_, err = fmt.Scan(&height)
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка при вводе роста: %v", err)
+	}
+
+	fmt.Print("Вес: ")
+	_, err = fmt.Scan(&weight)
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка при вводе веса: %v", err)
+	}
+
+	return height, weight, nil
 }
